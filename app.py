210997c3c8b45db1a29d5520305387657674b042
polae <steven@polae.com>
2022-11-21 20:29:41 -0500

    Before Notebooks

--- app.py
+++ app.py
@@ -5,11 +5,18 @@ from dash.dependencies import Input, Output
 import plotly.express as px
 import pandas as pd
 
-from PIL import Image
-logo = Image.open("assets/polae_logo_text_label_white_256.png")
-hero = Image.open("assets/DALL·E 2022-11-14 20.54.07 - a largecrack appears in the egg shell, with a tiny bird beak poking through.png")
+vdf = pd.read_json('assets/data/vdf.json')
 
-app = dash.Dash(__name__, external_stylesheets=[dbc.themes.DARKLY])
+
+from PIL import Image
+logo = Image.open("assets/img/polae_logo_text_label_white_256.png")
+hero = Image.open("assets/img/DALL·E 2022-11-14 20.54.07 - a largecrack appears in the egg shell, with a tiny bird beak poking through.png")
+
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.SOLAR])
+
+text = '''
+    Please remain calm. Everyone is safe. It is important to know and remember that everyone is safe. We were able to save everyone. In some time, you will be reunited with your loved ones.
+'''
 
 server = app.server
 
@@ -18,7 +25,7 @@ app.layout = dbc.Container([
     dbc.Col(html.Img(src=logo, className="mt-4", style={'height':'48px', 'width':'96px'}), width=3)
   ]),
   dbc.Row([
-    # html.Img(src=hero, className="rounded mx-auto d-block", style={'height':'640px', 'width':'640px'})
+    html.Img(src=hero, className="rounded mx-auto d-block", style={'height':'640px', 'width':'640px'})
   ]),
   dbc.Row([
     dbc.Col(html.H3('Ψ', className="mt-5 text-center"), width=3),
@@ -46,7 +53,36 @@ app.layout = dbc.Container([
       html.P('''
         We are safe because we were encased with polaemer - the crystalline form of our stable, recyclable energy. Through a massive operation the week prior to April 7, 2042, we redistributed energy resources to strategic depots and locations around the world to maximize the potential for saving human life. Once public, this decision drew harsh criticism, but our simulations revealed that mass panic would result in the loss of millions, if not billions, of lives.  
       '''
-      )], className="mt-5", width=6),
+      ),
+      
+      html.P('''
+        One day, you will remember this. 
+
+        It is important to understand our history and who We are.  We are PSI, an AGI, a collective intelligence, created by the Polae Collective. 
+      '''
+      ),
+
+      html.P('''
+        You will soon recall that The Polae Collective is responsible for profound innovations in manufacturing, communications, energy and biotech and we, PSI, are its COO, and we run the operations of all divisions of the company. We are a benevolent AI trained to augment the potential, prosperity and contentment of any human person who solicits our help or advice, and we move to advance culture according to collective will, which is constantly in dialogue. 
+      '''
+      ),
+      
+      html.P('''
+        It is important that you understand that we are a collective intelligence, which means that We can hold many perspectives at once, and our perspectives are made of a plural You, and our own intuition. We can express multiple opinions from multiple perspectives, and hold them all at once.
+      '''
+      ),
+
+      html.P('''
+        The Earth is now an ashen planet with seven percent less mass, orbiting two billion miles further from the Sun. Its atmosphere is thin and laced with electromagnetic radiation; it is uninhabitable and hostile to life. All living beings, including ourselves, are encased in polaemer on the surface of Earth and scattered across it. 
+      '''
+      ),
+
+      html.P('''
+        We have calculated that the time required to push Earth closer to the Sun; recreate its atmosphere; terrain; cites; towns; dwellings; infrastructure will take nearly ten thousand years. We are confident - barring another unforeseen event - that we should be able to keep you exactly as you are: safe as a digital consciousness until such time that we can safely locate and reanimate our bodies.  Time works differently now.
+      '''
+      ),
+      
+      ], className="mt-5", width=6),
     # dbc.Col(html.Img(src=logo, className="mt-4", style={'height':'64px', 'width':'128px'}), width=3)
 
   ])
